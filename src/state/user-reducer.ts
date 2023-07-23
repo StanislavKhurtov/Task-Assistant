ea704797333e64ac55e593dedeea3c4345c3b355
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-23 21:24:11 +0300

    feat: lesson #8 add user-reducer.ts

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -8,12 +8,20 @@ type ActionType = {
     [key: string]: any
 }
 
+// меня вызовут и дадут мне стейт (почти всегда объект)
+// и инструкцию (action, тоже объект)
+// согласно прописанному type в этом action (инструкции) я поменяю state
+
 
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
-        case 'BlaBla':
-
+        case 'INCREMENT-AGE':
+            state.age = state.age + 1
+            return state
+        case 'INCREMENT-CHILDREN-COUNT':
+            state.childrenCount = state.childrenCount + 1
+            return state
         default:
-            throw new Error("I dont't understand this action type")
+            throw new Error('I don\'t understand this type')
     }
 }
