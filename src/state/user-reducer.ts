760621adb45531d26b007c7845dc7f17cfe218eb
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-23 21:55:52 +0300

    feat: lesson #8 add action

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -13,12 +13,14 @@ type ActionType = {
 // согласно прописанному type в этом action (инструкции) я поменяю state
 
 
-export const userReducer = (state: StateType, action: ActionType):StateType => {
+export const userReducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            return {...state,age: state.age + 1 }
+            return {...state, age: state.age + 1}
         case 'INCREMENT-CHILDREN-COUNT':
-           return {...state, childrenCount: state.childrenCount + 1};
+            return {...state, childrenCount: state.childrenCount + 1};
+        case 'CHANGE-NAME':
+            return {...state, name: action.newName}
         default:
             throw new Error('I don\'t understand this type')
     }
