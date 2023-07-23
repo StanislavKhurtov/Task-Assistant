ecb9c43fbce9a0052d46b99d23b8b0148de2a3e2
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-23 21:24:29 +0300

    feat: lesson #8 add user-reducer-test

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -1,44 +1,16 @@
-import { userReducer } from './user-reducer';
+import { userReducer } from './user-reducer'
 
-describe('userReducer', () => {
-    it('should handle BlaBla action', () => {
-        const initialState = {
-            age: 30,
-            childrenCount: 2,
-            name: 'John',
-        };
+test('user reducer should increment only age', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
 
-        const action = {
-            type: 'BlaBla',
-            // Дополнительные свойства, если требуются для теста
-        };
+    const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
 
-        const newState = userReducer(initialState, action);
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(2)
+})
 
-        // Проверяем, что состояние после действия 'BlaBla' соответствует вашим ожиданиям
-        expect(newState).toEqual({
-            age: 30,
-            childrenCount: 2,
-            name: 'John',
-            // Дополнительные свойства, если вы их изменяете в редюсере
-        });
-    });
+test('user reducer should increment only childrenCount', () => {
+    const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
 
-    it('should throw an error for unknown action type', () => {
-        const initialState = {
-            age: 30,
-            childrenCount: 2,
-            name: 'John',
-        };
 
-        const action = {
-            type: 'UnknownAction',
-            // Дополнительные свойства, если требуются для теста
-        };
-
-        // Проверяем, что при неизвестном типе действия будет выброшено исключение
-        expect(() => {
-            userReducer(initialState, action);
-        }).toThrow("I don't understand this action type");
-    });
-});
+})
