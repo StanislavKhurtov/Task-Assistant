5c0c569f5d1f7d95da57af188581251a9a9c6e7a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-24 16:07:28 +0300

    feat: lesson #9 add  test changeTaskStatus

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -58,6 +58,28 @@ test('correct task should be added to correct array', () => {
     expect(endState['todolistId2'][0].isDone).toBe(false)
 });
 
+test('status of specified task should be changed', () => {
+    const startState: TaskStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
+
+    const action = changeTaskStatusAC('todolistId2', '2', false,)
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todolistId2'][1].isDone).toBe(false)
+    expect(endState['todolistId1'][1].isDone).toBe(true)
+});
+
 
 
 
