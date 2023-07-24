ebac90678af84daefc7a0851a779ff803971bec0
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-24 11:24:53 +0300

    feat: lesson #8 add case change todolist title

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -32,6 +32,8 @@ export const todolistsReducer = (state: Array<TodolistType>, action: ActionTypes
             return state.filter(el => el.id !== action.id)
         case 'ADD-TODOLIST':
             return [...state, {id: v1(), title: action.title, filter: "all"}]
+        case 'CHANGE-TODOLIST-TITLE':
+            return state.map(el => el.id === action.id ? {...el, title: action.title} : el)
 
         default:
             throw new Error("I don't understand this type")
@@ -46,5 +48,8 @@ export const addTodolistAC = (title: string): AddTotodlistActionType => {
     return {type: 'ADD-TODOLIST', title};
 }
 
+export const changeTodolistTitleAC = (id: string, title: string): ChangeTotodlistTitleActionType => {
+    return { type: 'CHANGE-TODOLIST-TITLE', id: id, title: title};
+}
 
 
