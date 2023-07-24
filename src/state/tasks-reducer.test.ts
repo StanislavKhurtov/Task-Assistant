8e14599013ae0c4484ee78be8aa88ac147079fed
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-24 16:08:10 +0300

    feat: lesson #9 add  changeTaskTitle

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -80,7 +80,28 @@ test('status of specified task should be changed', () => {
     expect(endState['todolistId1'][1].isDone).toBe(true)
 });
 
+test('title of specified task should be changed', () => {
+    const startState: TaskStateType = {
+        'todolistId1': [
+            {id: '1', title: 'CSS', isDone: false},
+            {id: '2', title: 'JS', isDone: true},
+            {id: '3', title: 'React', isDone: false}
+        ],
+        'todolistId2': [
+            {id: '1', title: 'bread', isDone: false},
+            {id: '2', title: 'milk', isDone: true},
+            {id: '3', title: 'tea', isDone: false}
+        ]
+    }
 
+
+    const action = changeTaskTitleAC('todolistId2', '2', "sugar")
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState['todolistId2'][1].title).toBe("sugar")
+    expect(endState['todolistId1'][1].title).toBe('JS')
+})
 
 
 
