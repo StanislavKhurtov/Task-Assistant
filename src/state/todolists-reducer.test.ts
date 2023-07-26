0c7416f556c324b842d633709bc8d1f1b8d8a738
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-26 15:40:47 +0300

    feat: lesson #9 add  todolists-tasks-reducer.test.ts

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -35,7 +35,7 @@ test('correct todolist should be added', () => {
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
 
-    const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle))
+    const endState = todolistsReducer(startState, addTodolistAC(v1(),newTodolistTitle))
 
     expect(endState.length).toBe(3)
     expect(endState[2].title).toBe(newTodolistTitle)
