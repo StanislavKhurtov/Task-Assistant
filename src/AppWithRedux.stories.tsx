7064fe93a32fe7180e3f7032402bf4fbdeb896ad
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-09 12:11:34 +0300

    feat: lesson #12 add decorator story book

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -8,13 +8,10 @@ import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator
 export default {
     title: 'AppWithRedux Component',
     component: AppWithRedux,
-    decorators: ReduxStoreProviderDecorator,
+    decorators: [ReduxStoreProviderDecorator],
 }
 
 
 export const AppWithReduxBaseExample = () => {
-    return <Provider store={store}>
-        <AppWithRedux/>
-    </Provider>
-
+    return <AppWithRedux/>
 }
--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -3,9 +3,6 @@ import {Provider} from "react-redux";
 import {store} from "../state/store";
 
 
-
-
-
-export const ReduxStoreProviderDecorator = (store:any) => {
-    return <Provider store={store}>story()</Provider>;
+export const ReduxStoreProviderDecorator = (story:any) => {
+    return <Provider store={store}>{story()}</Provider>;
 }
