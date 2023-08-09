f8e9fe58702371a8030c3e1c673924f3a99a4c06
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-09 13:45:46 +0300

    feat: lesson #12 add test add file integration

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {AppWithRedux} from "./AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./state/store";
 import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
 
