bb89780e5d000bacef050385635c5b0fec7f6614
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-09 09:13:17 +0300

    refactor: lesson #12 change structure ? add action

--- src/AddItemForm.stories.tsx
+++ src/AddItemForm.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {action} from '@storybook/addon-actions'
 import {AddItemForm} from "./AddItemForm";
 
 
@@ -7,7 +8,8 @@ export default {
     component: AddItemForm
 }
 
+const callback = action('Button "add" was pressed inside the form')
 
 export const AddItemFormBaseExample = (props: any) => {
-    return <AddItemForm addItem={(title:string)=>{alert(title)}} />
+    return <AddItemForm addItem={callback} />
     }
