64b47b42b776d2ccc681f12ac1aa22ccb15a8b5a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-13 19:07:03 +0300

    feat: lesson #12 change structure AddItemForm

--- src/AddItemForm.stories.tsx
+++ src/AddItemForm.stories.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import type {Meta, StoryObj} from '@storybook/react';
 import {action} from '@storybook/addon-actions'
 import {AddItemForm} from "./AddItemForm";
 
 
-export default {
-    title: 'AddItemForm Component',
-    component: AddItemForm
-}
+const meta: Meta<typeof AddItemForm> = {
+    title: 'TODOLISTS/AddItemForm',
+    component: AddItemForm,
+    tags: ['autodocs'],
+    argTypes: {
+        addItem: {
+            description: 'Button clicked inside form',
+            action: 'clicked'
+        }
+    },
+};
 
-const callback = action('Button "add" was pressed inside the form')
+export default meta;
+type Story = StoryObj<typeof AddItemForm>;
 
-export const AddItemFormBaseExample = (props: any) => {
-    return <AddItemForm addItem={callback} />
-    }
+export const AddItemFormStory: Story = {
+    args: {
+        addItem: action('Button clicked inside form')
+    },
+};
