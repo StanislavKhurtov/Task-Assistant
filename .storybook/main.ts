64f3a866b707391eb84d93c8c317b02303f3e8c5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-13 19:03:16 +0300

    feat: lesson #12 inst storybook 7.2.3

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,39 +1,14 @@
-import type {StorybookConfig} from "@storybook/react-webpack5";
-
-
+import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-create-react-app",
-        "@storybook/addon-onboarding",
-        "@storybook/addon-interactions",
-        {
-            name: '@storybook/addon-storysource',
-            options: {
-                rule: {
-                    test: [/\.stories\.tsx?$/],
-                },
-                loaderOptions: {
-                    prettierConfig: {
-                        printWidth: 80,
-                        singleQuote: false,
-                        options: {
-                          parser: 'typescript',
-                        }
-                    },
-                },
-            },
-        },
-    ],
-    framework: {
-        name: "@storybook/react-webpack5",
-        options: {},
-    },
-    docs: {
-        autodocs: "tag",
-    },
-    staticDirs: ["..\\public"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-onboarding", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: "tag"
+  },
+  staticDirs: ["..\\public"]
 };
 export default config;
