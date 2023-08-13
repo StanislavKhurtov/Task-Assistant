e04e61b89820a9ab6e2551e2f46489f39896faf6
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-13 19:16:59 +0300

    feat: lesson #12 change structure AppWithRedux.stories.tsx

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import type {Meta, StoryObj} from '@storybook/react';
 import {AppWithRedux} from "./AppWithRedux";
 import {ReduxStoreProviderDecorator} from "./stories/ReduxStoreProviderDecorator";
 
 
-export default {
-    title: 'AppWithRedux Component',
+
+
+const meta: Meta<typeof AppWithRedux> = {
+    title: 'TODOLISTS/AppWithRedux',
     component: AppWithRedux,
-    decorators: [ReduxStoreProviderDecorator],
-}
 
+    tags: ['autodocs'],
 
-export const AppWithReduxBaseExample = () => {
-    return <AppWithRedux/>
-}
+    decorators: [ReduxStoreProviderDecorator]
+};
+
+export default meta;
+type Story = StoryObj<typeof AppWithRedux>;
+
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const AppWithReduxStory: Story = {}
+
