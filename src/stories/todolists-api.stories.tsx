018fe2c7429a2b4116c655902a20ef675da6a8a7
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-15 19:15:47 +0300

    feat:lesson #13 todolists-api.stories change structure SetTodolist GetTodolist CreateTodolist

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,14 +1,27 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
 }
 
-export const GetTodolists = () => {
+const settings = {
+    withCredentials:true,
+    headers: {
+        'API-KEY': '407432d0-8fb3-48b8-9578-523f43ccc02f'
+    },
+}
+
+export const GetTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',settings)
+            .then((res) => {
+
+                setState( res.data)
+            })
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -16,6 +29,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',{title: 'Stanislav Todolist'},settings)
+
+            .then((res) => {
+                setState( res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
