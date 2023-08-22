468853ff6e9ebd57e9bca2faf6255858fb597675
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-22 22:12:04 +0300

    feat: lesson #13 task API

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -7,7 +7,6 @@ type TodolistType = {
     title: string
 }
 
-
 export type ResponseType<D = {}> = {
     resultCode: number
     messages: Array<string>
@@ -29,7 +28,7 @@ export const todolistAPI = {
         return instance.get<Array<TodolistType>>(`todo-lists/`)
     },
     createTodolist(title: string) {
-        return instance.post<ResponseType<{ item: TodolistType}>>(`todo-lists/`, {title: title})
+        return instance.post<ResponseType<{ item: TodolistType }>>(`todo-lists/`, {title: title})
     },
     deleteTodolist(todolistId: string) {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
@@ -37,4 +36,18 @@ export const todolistAPI = {
     updateTodolist(todolistId: string, title: string) {
         return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title: title})
     },
+    getTask(todolistId: string) {
+        return instance.get(`todo-lists/${todolistId}/tasks`)
+    },
+    createTask(todolistId: string, title: string) {
+        return instance.post(`todo-lists/{todolistId}/tasks`, {title: title})
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete(`todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+    updateTask(todolistId: string, taskId: string, title: string) {
+        return instance.put(`todo-lists/${todolistId}/tasks/${taskId}`, {title: title})
+    },
+
+
 }
