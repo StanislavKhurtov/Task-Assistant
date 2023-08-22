c9cf1579e95b7cabe8677bdf7ba01423c05a94db
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-22 18:58:24 +0300

    feat:lesson #13 add todolists-api.stories.tsx

--- integration/task.test.js
+++ integration/task.test.js
@@ -1,7 +1,7 @@
 describe('task', () => {
     it('base example, visually looks correct', async () => {
 
-        await page.goto('http://localhost:6006/iframe.html?args=&id=todolists-task--task-is-done-story&viewMode=story',
+        await page.goto('http://localhost:9009/iframe.html?args=&id=todolists-task--task-is-done-story&viewMode=story',
             {waitUntil: "networkidle2"});
 
         const image = await page.screenshot();
