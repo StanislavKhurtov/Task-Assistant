140641c5f83a753960dd992a1e6c7bc0fb827bda
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-23 20:22:25 +0300

    feat:lesson #13-03 change structure

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-type TodolistType = {
+export type TodolistType = {
     id: string
     addedDate: string
     order: number
@@ -14,21 +14,37 @@ export type ResponseType<D = {}> = {
     data: D
 }
 
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
+
 
 export type TaskType = {
     description: string
     title: string
-    status: number
-    priority: number
+    status: TaskStatuses
+    priority: TaskPriorities
     startDate: string
     deadline: string
     id: string
-    todoListId: string
+    todolistId: string
     order: number
     addedDate: string
+
 }
 
-type GetTasksResponse = {
+export type GetTasksResponse = {
     items: TaskType[]
     totalCount: number
     error: string | null
@@ -77,6 +93,5 @@ export const todolistAPI = {
     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
         return instance.put<ResponseType<TaskType>>(`todo-lists/${todolistId}/tasks/${taskId}`, model)
     },
-
 
 }
