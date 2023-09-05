5f9081366b9b99baf48b344d03c86fcd4cdc17de
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-05 20:47:49 +0300

    feat: lesson14 part 2 change structure

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -1,13 +1,16 @@
 import {v1} from "uuid";
 import {
-    addTodolistAC,
     changeTodolistFilterAC,
     ChangeTodolistFilterActionType,
     changeTodolistTitleAC,
-    ChangeTodolistTitleActionType, FilterValueType,
-    removeTodolistAC, TodolistDomainType,
+    ChangeTodolistTitleActionType,
+    FilterValueType,
+    removeTodolistAC,
+    setTodolistsAC,
+    TodolistDomainType,
     todolistsReducer
 } from "./todolist-reducer"
+import {tasksReducer} from "./task-reducer";
 
 
 test('correct todolist should be removed', () => {
@@ -25,7 +28,7 @@ test('correct todolist should be removed', () => {
     expect(endState[0].id).toBe(todolistId2)
 });
 
-test('correct todolist should be added', () => {
+/*test('correct todolist should be added', () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
@@ -40,7 +43,7 @@ test('correct todolist should be added', () => {
 
     expect(endState.length).toBe(3)
     expect(endState[2].title).toBe(newTodolistTitle)
-});
+});*/
 
 test('correct todolist should change its name', () => {
     let todolistId1 = v1()
@@ -78,4 +81,18 @@ test('correct filter of todolist should be changed', () => {
 
     expect(endState[0].filter).toBe('all')
     expect(endState[1].filter).toBe(newFilter)
+});
+
+test('empty should be added when we set todolists', () => {
+    const action = setTodolistsAC([
+        {id: '1', title: 'What to learn', addedDate: '', order: 0},
+        {id: '2', title: 'What to buy', addedDate: '', order: 0}
+    ])
+
+    const endState = tasksReducer({}, action)
+    const keys = Object.keys(endState)
+
+    expect(keys.length).toBe(2)
+    expect(endState['1']).toEqual([])
+    expect(endState['2']).toEqual([])
 });
