46cf53d7502e349619a381301ebba9b9388f9528
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-08 20:14:45 +0300

    refactor: lesson 15 -1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {AppWithRedux} from "./app/AppWithRedux";
+import {App} from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>,
     document.getElementById('root'));
