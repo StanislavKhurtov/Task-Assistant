1f5a1b6ce937fc6deb6f87ec4cc7fbc60b0d02ff
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-13 21:03:29 +0300

    feat: lesson 16 validation

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -11,15 +11,29 @@ import {
 } from "@material-ui/core";
 import {useFormik} from "formik";
 
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
 
 export const Login = () => {
-
 
     const formik = useFormik({
         initialValues: {
             email: '',
             password: '',
             rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            return errors
         },
         onSubmit: values => {
             alert(JSON.stringify(values));
@@ -49,14 +63,16 @@ export const Login = () => {
                             onChange={formik.handleChange}
                             value={formik.values.email}
                         />
+                        {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
                         <TextField
                             type="password"
                             label="Password"
                             margin="normal"
-                            name='email'
+                            name='password'
                             onChange={formik.handleChange}
-                            value={formik.values.email}
+                            value={formik.values.password}
                         />
+                        {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : null}
                         <FormControlLabel
                             label={'Remember me'}
                             control={<Checkbox
