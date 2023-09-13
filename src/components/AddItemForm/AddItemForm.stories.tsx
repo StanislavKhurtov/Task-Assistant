7964862a3f41b2ee04a32dc4b4c0ee0724f44cd1
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-13 13:28:12 +0300

    feat: lesson 15 -3 change structure

--- src/components/AddItemForm/AddItemForm.stories.tsx
+++ src/components/AddItemForm/AddItemForm.stories.tsx
@@ -1,9 +1,7 @@
 import {Meta, StoryObj} from "@storybook/react";
 import {action} from '@storybook/addon-actions'
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {IconButton, TextField} from "@material-ui/core";
-import {AddBox} from "@material-ui/icons";
-import {AddItemForm, AddItemFormPropsType} from "./AddItemForm";
+import React from "react";
+import {AddItemForm} from "./AddItemForm";
 
 const meta: Meta<typeof AddItemForm> = {
     title: 'TODOLISTS/AddItemForm',
@@ -21,57 +19,10 @@ export default meta;
 type Story = StoryObj<typeof AddItemForm>;
 
 
-export const AddItemFormStory: Story = {
-    args: {
-        addItem: action('Clicked button inside form')
-    }
+export const AddItemFormDisableExample = (props: any) => {
+    return (<AddItemForm disabled={true}
+        addItem={action('Button inside form clicked')}
+/>)
 }
 
-const AddItemFormWithError = (args: AddItemFormPropsType) => {
-    let [newTitle, setNewTitle] = useState("")
-    let [error, setError] = useState<string | null>("Title is requared")
 
-    const addItem = () => {
-        if (newTitle.trim() !== "") {
-            args.addItem(newTitle.trim())
-            setNewTitle("")
-        } else {
-            setError("Title is requared");
-        }
-    };
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(error !== null) {
-            setError(null)
-        }
-        if (newTitle.trim() !== "") {
-            if (e.key === "Enter") {
-                args.addItem(newTitle.trim())
-                setNewTitle("")
-            }
-        }
-    };
-
-    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value);
-
-    return (
-        <div>
-            <TextField
-                variant="outlined"
-                label={"Type value"}
-                value={newTitle}
-                onChange={onNewTitleChangeHandler}
-                onKeyPress={onKeyPressHandler}
-                error={!!error}
-                helperText={error}
-            />
-            <IconButton onClick={addItem} color={'primary'}>
-                <AddBox />
-            </IconButton>
-        </div>
-    );
-}
-
-export const AddItemFormWithErrorStory: Story = {
-    render: args => <AddItemFormWithError addItem={args.addItem} />
-}
