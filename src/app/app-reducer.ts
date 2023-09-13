429ed5754d26739bfbd16672d21ca6b1a0dd012f
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-13 18:11:37 +0300

    feat: lesson 15 -3 change structure

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -4,30 +4,30 @@ export type InitialStateType = {
 }
 export const initialstate: InitialStateType = {
     status: 'idle',
-    error: 'Some Errorrrrrrr'
+    error: null
 }
 
 export const appReducer = (state: InitialStateType = initialstate, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case 'APP/SET-STATUS':
+        case 'SET-STATUS':
             return {...state, status: action.status}
-        case 'APP/SET-ERROR':
+        case 'SET-ERROR':
             return {...state, error: action.error}
         default:
             return {...state}
     }
 }
 
-export const setErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-export const setStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error: string | null) => ({type: 'SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'SET-STATUS', status} as const)
 
 
 // types
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
-export type SetErrorActionType = ReturnType<typeof setErrorAC>
-export type SetStatusActionType = ReturnType<typeof setStatusAC>
+export type SetErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetStatusActionType = ReturnType<typeof setAppStatusAC>
 type ActionsType =
     SetErrorActionType
     | SetStatusActionType
