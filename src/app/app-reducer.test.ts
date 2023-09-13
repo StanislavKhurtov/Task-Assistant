c92c4236f8101453b2c5570dd77fd85fc8770f0b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-13 18:11:24 +0300

    feat: lesson 15 -3 change structure

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -1,7 +1,8 @@
-import {appReducer, InitialStateType, setErrorAC, setStatusAC} from "./app-reducer";
+import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from "./app-reducer";
 
 
 let startState: InitialStateType;
+
 beforeEach(() => {
     startState = {
         error: null,
@@ -11,14 +12,14 @@ beforeEach(() => {
 
 test('correct error message should be set', () => {
 
-    const endState = appReducer(startState, setErrorAC('some error'))
+    const endState = appReducer(startState, setAppErrorAC('some error'))
 
     expect(endState.error).toBe('some error')
 })
 
 test('correct status message should be set', () => {
 
-    const endState = appReducer(startState, setStatusAC('loading'))
+    const endState = appReducer(startState, setAppStatusAC('loading'))
 
     expect(endState.status).toBe('loading')
 })
