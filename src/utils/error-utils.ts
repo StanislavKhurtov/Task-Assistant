bb6acdda4644db82de67613c8674d0d502f42e17
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-18 14:09:28 +0300

    fix: lesson 17 fix error

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,19 +1,19 @@
-import {setAppErrorAC, setAppStatusAC, SetErrorActionType, SetStatusActionType} from "../app/app-reducer";
+import {setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
 import {ResponseType} from "../api/todolist-api";
 import {Dispatch} from "redux";
 
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetErrorActionType | SetStatusActionType>) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
     if (data.messages.length) {
-        dispatch(setAppErrorAC(data.messages[0]))
+        dispatch(setAppErrorAC({error: data.messages[0]}))
     } else {
-        dispatch(setAppErrorAC('some error occurred'))
+        dispatch(setAppErrorAC({error: 'some error occurred'}))
     }
-    dispatch(setAppStatusAC('failed'))
+    dispatch(setAppStatusAC({status: 'failed'}))
 };
 
-export const handleServerNetworkError = (error: {messages:string}, dispatch: Dispatch<SetErrorActionType | SetStatusActionType>) => {
-    dispatch(dispatch(setAppErrorAC(error.messages ? error.messages : '')))
-    dispatch(setAppStatusAC('failed'))
+export const handleServerNetworkError = (error: {messages:string}, dispatch: Dispatch) => {
+    dispatch(dispatch(setAppErrorAC({error: error.messages ? error.messages : ''})))
+    dispatch(setAppStatusAC({status: 'failed'}))
 };
 
