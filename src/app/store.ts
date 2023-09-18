02f1e08331836ae8d98be5fa46690a6d3db473e5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-18 12:19:24 +0300

    feat: lesson 17 change structure store add configureStore Instead of createStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import {AnyAction, combineReducers} from "redux";
 import {tasksReducer} from "../features/Todolists/task-reducer";
 import {todolistsReducer} from "../features/Todolists/todolist-reducer";
 import thunk, {ThunkDispatch} from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {appReducer} from "./app-reducer";
 import {loginReducer} from "../features/Login/login-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 
 const rootReducer = combineReducers({
@@ -14,15 +15,18 @@ const rootReducer = combineReducers({
     login: loginReducer,
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+//export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
+})
 
 export type AppRootState = ReturnType<typeof rootReducer>
+export const useAppDispatch = () => useDispatch<ThunkType>()
+export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector;
 
 export type ThunkType = ThunkDispatch<AppRootState, any, AnyAction>
-
-export const useAppDispatch = () => useDispatch<ThunkType>()
-
-export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector;
 
 // @ts-ignore
 window.store = store
