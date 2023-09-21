31933b73742a3b6847e52b44ad08d35bbde92b4a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-21 11:38:04 +0300

    refactor: change structure app-reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,10 +1,9 @@
-import {Dispatch} from "redux";
-import {setIsLoggedInAC} from "../features/Login/auth-reducer";
-import {authAPI} from "../api/todolist-api";
-import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
+import {authAPI} from "api/todolist-api";
+import {handleServerAppError, handleServerNetworkError} from "utils/error-utils";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {todolistsActions} from "../features/Todolists/todolist-reducer";
+import {todolistsActions} from "features/Todolists/todolist-reducer";
 import {AppThunk} from "./store";
+import {authActions} from "features/Login/auth-reducer";
 
 export type InitialStateType = {
     // происходит ли сейчас взайимодействие с сервером
@@ -36,26 +35,26 @@ const slice = createSlice({
     }
 })
 export const appReducer = slice.reducer;
-export const {setAppInitializedAC,setAppStatusAC,setAppErrorAC} = slice.actions;
+export const appActions = slice.actions;
 //thunk
-export const initializedAppTC = () => (dispatch: Dispatch) => {
+export const initializedAppTC = ():AppThunk => (dispatch) => {
     authAPI.me()
         .then(res => {
             if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: true}));
+                dispatch(authActions.setIsLoggedInAC({value: true}));
 
             } else {
             }
-            dispatch(setAppInitializedAC({isInitialized: true}));
+            dispatch(appActions.setAppInitializedAC({isInitialized: true}));
         })
 }
 export const logoutTC = (): AppThunk => (dispatch) => {
-    dispatch(setAppStatusAC({status:'loading'}))
+    dispatch(appActions.setAppStatusAC({status:'loading'}))
     authAPI.logout()
         .then(res => {
             if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: false}))
-                dispatch(setAppStatusAC({status: 'succeeded'}))
+                dispatch(authActions.setIsLoggedInAC({value: false}))
+                dispatch(appActions.setAppStatusAC({status: 'succeeded'}))
                 dispatch(todolistsActions.clearTodosDataAC({}))
             } else {
                 handleServerAppError(res.data, dispatch)
